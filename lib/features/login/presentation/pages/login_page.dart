786b17d7e89ebc9395cee82532514dbab95e6f7a
Rubén Darío Guarnizo Martínez <43024059+Rguarnizo@users.noreply.github.com>
2020-12-18 18:44:43 -0500

    Social Media Buttons and Create Account

--- lib/features/login/presentation/pages/login_page.dart
+++ lib/features/login/presentation/pages/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:moviliapp_mobile/core/widgets/buttons.dart';
 import 'package:moviliapp_mobile/core/widgets/text_form_field.dart';
 
 class LoginPage extends StatelessWidget {
@@ -22,10 +23,15 @@ class LoginPage extends StatelessWidget {
           decoration: backgroudDecorate(),
           child: SafeArea(
             child: Column(
-              mainAxisSize: MainAxisSize.max,                            
+              mainAxisSize: MainAxisSize.max,
               children: [
-                Image.asset('assets/Images/TMLogo.png',width: 150,height: 150,),
+                Image.asset(
+                  'assets/Images/TMLogo.png',
+                  width: 150,
+                  height: 150,
+                ),
                 _formLogin(),
+                _createAccount(),
               ],
             ),
           ),
@@ -36,11 +42,11 @@ class LoginPage extends StatelessWidget {
 
   backgroudDecorate() {
     return BoxDecoration(
-        gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: [Colors.red, Colors.red[900]]),
-        );
+      gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [Colors.red, Colors.red[900]]),
+    );
   }
 
   _formLogin() {
@@ -48,20 +54,89 @@ class LoginPage extends StatelessWidget {
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(16),
         color: Colors.white,
-        shape: BoxShape.rectangle,        
-      ),      
+        shape: BoxShape.rectangle,
+      ),
       margin: EdgeInsets.only(top: 15),
       padding: EdgeInsets.all(30),
-      height: height*(1-0.6),
-      width: width*(1-0.1),      
+      height: height * (1 - 0.6),
+      width: width * (1 - 0.1),
       child: Column(
         children: [
-          Text('Iniciar Sesión',style: TextStyle(fontSize: 18),),
-          SizedBox(height: 30,),
-          TextFormFieldApp(hint: 'Ingrese su correo',icon: Icons.email_outlined,),
-          TextFormFieldApp(hint:'Ingrese su contraseña',icon: Icons.lock_outline,obscureText: true,)
+          Text(
+            'Iniciar Sesión',
+            style: TextStyle(fontSize: 18),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          TextFormFieldApp(
+            hint: 'Ingrese su correo',
+            icon: Icons.email_outlined,
+          ),
+          TextFormFieldApp(
+            hint: 'Ingrese su contraseña',
+            icon: Icons.lock_outline,
+            obscureText: true,
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          OutlinedButton.icon(
+            onPressed: () {},
+            icon: Icon(Icons.chevron_right, size: 18),
+            label: Text(
+              "Iniciar Sesión",
+              style: TextStyle(fontSize: 18),
+            ),
+          )
         ],
       ),
+    );
+  }
+
+  _createAccount() {
+    return Stack(
+      overflow: Overflow.visible,
+      alignment: Alignment.center,
+      children: [
+      Container(
+        padding: EdgeInsets.all(15),
+        margin: EdgeInsets.only(top: 20),
+        height: height * (0.2),
+        width: width * (1 - 0.1),
+        child: _createAccountContent(),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(16),
+        ),
+      ),
+       Positioned(bottom: -20,child:_socialMedia(),)
+    ]);
+  }
+
+  _socialMedia() {
+    return Row(children: [
+        FlatButton(onPressed: (){},child: Image.asset('assets/Images/FB.png',height: 50,width: 50,),),
+        FlatButton(onPressed: (){},child: Image.asset('assets/Images/Twitter.png',height: 50,width: 50,),),
+        FlatButton(onPressed: (){},child: Image.asset('assets/Images/YT.png',height: 50,width: 50,),),
+        FlatButton(onPressed: (){},child: Image.asset('assets/Images/Inst.png',height: 50,width: 50,),),
+        
+      ]);
+  }
+
+  _createAccountContent() {
+    return Column(
+      children: [
+        Text('¿No tienes cuenta? Crea una...', style: TextStyle(fontSize: 18),),
+        SizedBox(height: 15,),
+        OutlinedButton.icon(
+            onPressed: () {},
+            icon: Icon(Icons.chevron_right, size: 18),
+            label: Text(
+              "Crear una cuenta",
+              style: TextStyle(fontSize: 18),
+            ),),
+      ],
     );
   }
 }

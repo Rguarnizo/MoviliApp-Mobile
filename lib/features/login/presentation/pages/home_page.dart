f6eb934ddb8a7b4d187765df9e06950782f1bdf6
Rubén Darío Guarnizo Martínez <43024059+Rguarnizo@users.noreply.github.com>
2020-12-19 23:16:25 -0500

    Home desing development (Posible change...)

--- lib/features/login/presentation/pages/home_page.dart
+++ lib/features/login/presentation/pages/home_page.dart
@@ -13,31 +13,69 @@ class HomePage extends StatelessWidget {
       width: width,
       color: Colors.red,
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Container(                
+          Container(
             width: width,
             padding: EdgeInsets.all(30),
             child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,              
+              mainAxisAlignment: MainAxisAlignment.start,
               children: [
-                Stack(           
-                  alignment: Alignment.bottomCenter,   
-                  children: [
-                  CircleAvatar(radius: 50,child: Text('perfilFoto'),),
-                  Text('Name Here',style: TextStyle(color: Colors.white),)
-                  ]
+                Stack(alignment: Alignment.bottomCenter, children: [
+                  CircleAvatar(
+                    radius: 50,
+                    child: Text('perfilFoto'),
+                  ),
+                  Text(
+                    'Name Here',
+                    style: TextStyle(color: Colors.white),
+                  )
+                ]),
+                SizedBox(
+                  width: 10,
                 ),
-                SizedBox(width: 10,),
-                Column(                  
-                  mainAxisSize: MainAxisSize.max,                  
-                  children: [
-                  Text('Saldo',style: TextStyle(color: Colors.white,fontSize: 18),),
-                  Text('\$Value',style: TextStyle(color: Colors.white,fontSize: 18),)
-                  ]
-                ),                
+                Column(mainAxisSize: MainAxisSize.max, children: [
+                  Text(
+                    'Saldo',
+                    style: TextStyle(color: Colors.white, fontSize: 18),
+                  ),
+                  Text(
+                    '\$Value',
+                    style: TextStyle(color: Colors.white, fontSize: 18),
+                  )
+                ]),
               ],
             ),
           ),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: Row(
+              children: [
+                Text('Hola, ',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 15,
+                        color: Colors.white)),
+                Text(
+                  'Nombre',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                      color: Colors.white),
+                )
+              ],
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.only(left: 40),
+              child: Text(
+            'Ven y viaja con nosotros!',
+            style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 16,
+                color: Colors.grey[300],
+                ),
+          )),
         ],
       ),
     );

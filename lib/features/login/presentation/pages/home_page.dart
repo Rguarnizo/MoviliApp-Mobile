189697b1938d9def0e38d6cae8c841ee3592faca
Rubén Darío Guarnizo Martínez <43024059+Rguarnizo@users.noreply.github.com>
2020-12-19 22:34:14 -0500

    Curved Navigation Bar

--- lib/features/login/presentation/pages/home_page.dart
+++ lib/features/login/presentation/pages/home_page.dart
@@ -1,19 +1,31 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 
 
 class HomePage extends StatelessWidget {
   const HomePage({Key key}) : super(key: key);
-
+  
   @override
   Widget build(BuildContext context) {
+    final screenSize = MediaQuery.of(context).size;
+    final height = screenSize.height;
+    final widht = screenSize.width;
+
     return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(
+      bottomNavigationBar: CurvedNavigationBar(
+        index: 1,
+        backgroundColor: Colors.red,
         items: [        
-          BottomNavigationBarItem(icon: Icon(Icons.directions_bus),label: ''),
-          BottomNavigationBarItem(icon: Icon(Icons.home_outlined),label: ''), 
-          BottomNavigationBarItem(icon: Icon(Icons.monetization_on),label: ''),        
-        ]
+           Icon(Icons.directions_bus,),
+           Icon(Icons.home_outlined),
+           Icon(Icons.monetization_on),
+        ],
+        onTap: (i){
+          
+        },
+        
         ),
+        body: Container(color: Colors.red,height: height,width: widht,),
     );
   }
 }

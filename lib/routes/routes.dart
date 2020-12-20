67f558698e99009709eea067e208af779dec9d10
Rubén Darío Guarnizo Martínez <43024059+Rguarnizo@users.noreply.github.com>
2020-12-19 20:36:41 -0500

    Create HomePage and route

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -3,11 +3,13 @@
 
 import 'package:flutter/material.dart';
 import 'package:moviliapp_mobile/features/login/presentation/pages/Login_page.dart';
+import 'package:moviliapp_mobile/features/login/presentation/pages/home_page.dart';
 import 'package:moviliapp_mobile/features/login/presentation/pages/register_page.dart';
 
 
 Map<String,WidgetBuilder> getRoutes() => {
     'Login'   : (BuildContext context)  => LoginPage(),
     'Register': (BuildContext context)  => RegisterPage(),
+    'Home'    : (BuildContext context)  => HomePage(),
     
 };

140478c9ce91cfa0a6bf62443f3f5b4dad4b99dc
Rubén Darío Guarnizo Martínez <43024059+Rguarnizo@users.noreply.github.com>
2020-12-20 21:31:27 -0500

    HomePage User Photo

--- lib/features/login/presentation/pages/home_page.dart
+++ lib/features/login/presentation/pages/home_page.dart
@@ -12,71 +12,78 @@ class HomePage extends StatelessWidget {
       height: height,
       width: width,
       color: Colors.red,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            width: width,
-            padding: EdgeInsets.all(30),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Stack(alignment: Alignment.bottomCenter, children: [
-                  CircleAvatar(
-                    radius: 50,
-                    child: Text('perfilFoto'),
+      child: SafeArea(
+              child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              margin: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(16),
+              ),
+              width: width,
+              padding: EdgeInsets.all(30),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Stack(alignment: Alignment.bottomCenter, children: [
+                    CircleAvatar(
+                      backgroundImage: NetworkImage('https://st3.depositphotos.com/13159112/17145/v/600/depositphotos_171453724-stock-illustration-default-avatar-profile-icon-grey.jpg'),
+                      radius: 50,                    
+                    ),
+                    Text(
+                      'Rubén Darío',
+                      style: TextStyle(fontSize: 18),
+                    )
+                  ]),
+                  SizedBox(
+                    width: 10,
                   ),
-                  Text(
-                    'Name Here',
-                    style: TextStyle(color: Colors.white),
-                  )
-                ]),
-                SizedBox(
-                  width: 10,
-                ),
-                Column(mainAxisSize: MainAxisSize.max, children: [
-                  Text(
-                    'Saldo',
-                    style: TextStyle(color: Colors.white, fontSize: 18),
-                  ),
-                  Text(
-                    '\$Value',
-                    style: TextStyle(color: Colors.white, fontSize: 18),
-                  )
-                ]),
-              ],
+                  Column(mainAxisSize: MainAxisSize.max, children: [
+                    Text(
+                      'Saldo',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                    Text(
+                      '\$Value',
+                      style: TextStyle(fontSize: 18),
+                    )
+                  ]),
+                ],
+              ),
             ),
-          ),
-          Container(
-            padding: EdgeInsets.symmetric(horizontal: 20),
-            child: Row(
-              children: [
-                Text('Hola, ',
+            Container(            
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Row(
+                children: [
+                  Text('Hola, ',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 15,
+                          color: Colors.white)),
+                  Text(
+                    'Nombre',
                     style: TextStyle(
                         fontWeight: FontWeight.bold,
-                        fontSize: 15,
-                        color: Colors.white)),
-                Text(
-                  'Nombre',
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16,
-                      color: Colors.white),
-                )
-              ],
+                        fontSize: 16,
+                        color: Colors.white),
+                  )
+                ],
+              ),
             ),
-          ),
-          Container(
-            padding: EdgeInsets.only(left: 40),
-              child: Text(
-            'Ven y viaja con nosotros!',
-            style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 16,
-                color: Colors.grey[300],
-                ),
-          )),
-        ],
+            Container(
+              padding: EdgeInsets.only(left: 40),
+                child: Text(
+              'Ven y viaja con nosotros!',
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                  color: Colors.grey[300],
+                  ),
+            )),
+          ],
+        ),
       ),
     );
   }

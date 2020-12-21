9c7f81cd060cb0490193e5a37b0aa25a6555b664
Rubén Darío Guarnizo Martínez <43024059+Rguarnizo@users.noreply.github.com>
2020-12-20 21:43:22 -0500

    Card Extrainfo Homepage

--- lib/features/login/presentation/pages/home_page.dart
+++ lib/features/login/presentation/pages/home_page.dart
@@ -53,35 +53,48 @@ class HomePage extends StatelessWidget {
                 ],
               ),
             ),
-            Container(            
-              padding: EdgeInsets.symmetric(horizontal: 20),
-              child: Row(
+            Container(
+              padding: EdgeInsets.all(20),
+              
+              margin: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(16),),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text('Hola, ',
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 15,
-                          color: Colors.white)),
-                  Text(
-                    'Nombre',
+                  Container(            
+                    padding: EdgeInsets.symmetric(horizontal: 20),
+                    child: Row(
+                      children: [
+                        Text('Hola, ',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 15,
+                                color: Colors.white)),
+                        Text(
+                          'Rubén Darío',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                              color: Colors.white),
+                        )
+                      ],
+                    ),
+                  ),
+                  Container(              
+                    padding: EdgeInsets.only(left: 40),
+                      child: Text(
+                    'Ven y viaja con nosotros!',
                     style: TextStyle(
                         fontWeight: FontWeight.bold,
                         fontSize: 16,
-                        color: Colors.white),
-                  )
+                        color: Colors.grey[300],
+                        ),
+                  )),
                 ],
               ),
             ),
-            Container(
-              padding: EdgeInsets.only(left: 40),
-                child: Text(
-              'Ven y viaja con nosotros!',
-              style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 16,
-                  color: Colors.grey[300],
-                  ),
-            )),
           ],
         ),
       ),
